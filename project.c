8a4a53849e47803a12dd5bca0e128f09d8ae984a
SweeTeaRex <Trey.m.adams92@gmail.com>
2024-09-12 19:57:02 -0500

    Update

---
+++ project.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Welcome to my text based adventure, created with C!\n");
+    printf("Type "next" to continue\n");
+    scanf(Input:)
+    /* code */
+    return 0;
+}
