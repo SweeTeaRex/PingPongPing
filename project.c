aac99149ff7427c7b23853a3988ffc1938ad7beb
SweeTeaRex <Trey.m.adams92@gmail.com>
2024-09-12 22:25:42 -0500

    Update

--- project.c
+++ project.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+
 int main(void)
 {
     printf("Welcome to my text based adventure, created with C!\n");
-    printf("Type "next" to continue\n");
-    scanf(Input:)
+    printf("Type 'next' to continue\n");
+    
+    char input[10];
+    scanf("%s", input);
+    
+    printf("%s\n", input);
     /* code */
     return 0;
 }
