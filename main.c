8befb81a320832ae79dc046fe65edf7d3474bc0a
Trey <trey.m.adams92@gmail.com>
2024-11-12 03:34:37 -0600

    Update

--- main.c
+++ main.c
@@ -36,7 +36,9 @@ int main(void)
     
 
     GameScreen currentScreen = StartScreen;
-    // StartScreen 
+
+    // ------StartScreen-------- 
+
     //programmers.png init
     Image programmers_image = LoadImage("programmers.png");
     Texture2D programmers_texture = LoadTextureFromImage(programmers_image);
@@ -45,23 +47,38 @@ int main(void)
     // init button
     Rectangle startbutton = { (screenWidth/2), (screenHeight/2), 500, 100};
 
-    float rectX = (screenWidth/2);
-    float rectY = (screenHeight/2);
     // button bounds
-    Rectangle startbtnbounds = { rectX-(startbutton.width/2), (rectY + 350), (float)startbutton.width, (float)startbutton.height};
+    Rectangle startbtnbounds = { ((screenWidth/2)-startbutton.width/2), ((screenHeight/2) + 350), (float)startbutton.width, (float)startbutton.height};
     // init mouse
     Vector2 mousePoint = { 0.0f, 0.0f };
 
-    // PlayScreen
+    // ----PlayScreen------
+
     // exit button
-    Rectangle exit_button = {0, 0, 20, 20};
-    Rectangle exit_button_bounds = {0, 0, exit_button.width, exit_button.height};
+    Rectangle exit_button = {0, 0, 100, 100};
+    Rectangle exit_button_bounds = {(screenWidth/2), 30, exit_button.width, exit_button.height};
     //exit.png init
     Image exit_image = LoadImage("exitdoor.png");
     ImageResize(&exit_image, 100, 100);
     Texture2D exit_texture = LoadTextureFromImage(exit_image);
     //free exit.png memory
     UnloadImage(exit_image);
+    //init bouncing ball
+    Vector2 ballPosition = { screenWidth/2.0f, screenHeight/2.0f };
+    Vector2 ballSpeed = { 5.0f, 4.0f };
+    int ballRadius = 20;
+    // pause menu init
+    bool pause = 0;
+    // frame counter
+    int framesCounter = 0;
+
+    // init paddles
+    Rectangle paddleLeft = { (screenWidth-(screenWidth/16)*15), (screenHeight/2), 25, 300};
+    Rectangle paddleRight = { (screenWidth-(screenWidth/16)*1), (screenHeight/2), 25, 300};
+    // paddle bounds
+    Rectangle paddleLeft_bounds = {0, 0, paddleLeft.width, paddleLeft.height};
+    Rectangle paddleRight_bounds = {0, 0, paddleRight.width, paddleRight.height };
+
 
     SetTargetFPS(60);
     
@@ -81,7 +98,19 @@ int main(void)
             }
             case PlayScreen:
             {
-                
+                if(CheckCollisionPointRec(mousePoint, exit_button_bounds) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+                {
+                    currentScreen = StartScreen;
+                }
+                if (IsKeyPressed(KEY_SPACE)) pause = !pause;
+
+                if (!pause)
+                {
+                    
+                    
+
+if ((ballPosition.x >= (GetScreenWidth() - ballRadius)) || (ballPosition.x <= ballRadius)) ballSpeed.x *= -1.0f;
+                    if ((ballPosition.y >= (GetScreenHeight() - ballRadius)) || (ballPosition.y <= ballRadius)) ballSpeed.y *= -1.0f;
                 break;
             }        
         }
@@ -96,10 +125,10 @@ int main(void)
                 DrawText("Welcome! \nThis is my first C program that I have made on my own. \nHave fun!", 2, 2, 50, BLACK);
                 // Used ChatGPT to help with centering button and image
                 
-                DrawRectangle(rectX-(startbutton.width/2), (rectY + 350), startbutton.width, startbutton.height, BLACK);
+                DrawRectangle((screenWidth/2)-(startbutton.width/2), ((screenHeight/2) + 350), startbutton.width, startbutton.height, BLACK);
                 // measure text width
                 int textWidth = MeasureText("START", 100);
-                DrawText("START", rectX-(textWidth/2), (rectY+350)+(startbutton.height/2) - (100/2), 100, WHITE);
+                DrawText("START", (screenWidth/2)-(textWidth/2), ((screenHeight/2)+350)+(startbutton.height/2) - (100/2), 100, WHITE);
             
                 // I used ChatGPT.com to figure out how to center the image 
                 DrawTexture(programmers_texture, ((screenWidth - programmers_texture.width)/2), ((screenHeight - programmers_texture.height)/2), WHITE);
@@ -110,9 +139,19 @@ int main(void)
             
             case PlayScreen:
             {
-                DrawText("Play Screen", (screenWidth/2), 0, 20, BLACK);
-
+                //Draw Exit Sign
+                DrawText("Click to Exit", (screenWidth/2), 0, 20, BLACK);
                 DrawTexture(exit_texture, (screenWidth/2), 30, WHITE);
+
+                //Draw ball
+                DrawCircleV(ballPosition, (float)ballRadius, MAROON);
+
+                // Draw Paddles
+                DrawRectangle(paddleLeft.x, paddleLeft.y, paddleLeft.width, paddleLeft.height, BLACK);
+                DrawRectangle(paddleRight.x, paddleRight.y, paddleRight.width, paddleRight.height, BLACK);
+
+
+
                 break;
 
 
