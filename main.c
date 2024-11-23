ad536223f115ca930b67db08ef8bd35375111d9d
Trey <trey.m.adams92@gmail.com>
2024-11-23 17:41:45 -0600

    update

--- main.c
+++ main.c
@@ -17,8 +17,7 @@
 #include <string.h>
 #include <raylib.h>
 
-typedef enum GameScreen {
-    
+typedef enum GameScreen {    
     StartScreen = 0,
     PlayScreen
 } GameScreen;
@@ -26,74 +25,86 @@ typedef enum GameScreen {
 
 int main(void)
 {
-    InitWindow(1500, 1000, "Game");
+    InitWindow(1500, 1000, "Game");    
 
-    
     // screen size init
     const int screenWidth = GetScreenWidth();
     printf("screenWidth: %i", screenWidth);
     const int screenHeight = GetScreenHeight();
     printf("screenHeight: %i", screenHeight);
+
     // init audio device
     InitAudioDevice();
-
     GameScreen currentScreen = StartScreen;
 
     // ------StartScreen-------- 
-
     //programmers.png init
     Image programmers_image = LoadImage("images/programmers.png");
     Texture2D programmers_texture = LoadTextureFromImage(programmers_image);
+
     //free memory from Image)
     UnloadImage(programmers_image);
+
     // init button
     Rectangle startbutton = { (screenWidth/2), (screenHeight/2), 500, 100};
 
     // button bounds
     Rectangle startbtnbounds = { ((screenWidth/2)-startbutton.width/2), ((screenHeight/2) + 350), (float)startbutton.width, (float)startbutton.height};
+
     // init mouse
     Vector2 mousePoint = { 0.0f, 0.0f };
 
     // ----PlayScreen------
-
     // init sound files
-    Sound startSound = LoadSound("sounds/01_Shop Channel Title.wav");
-    Sound playSound = LoadSound("sounds/02_Shop Channel.wav");
-    Sound ballbouncefx = LoadSound("sounds/Mario-coin-sound.wav");
+    Music startSound = LoadMusicStream("sounds/tokyo-cafe.wav");
+    Music playSound = LoadMusicStream("sounds/bop1.wav");
+    Sound ballbouncefx = LoadSound("sounds/whatever.wav");
+    
+    // adjust sound volume level
+    SetSoundVolume(ballbouncefx, 0.4);
     // check for if sound is already playing
     bool startSoundPlayed = false;
     bool playSoundPlayed = false;
+
     // exit button
     Rectangle exit_button = {0, 0, 100, 100};
     Rectangle exit_button_bounds = {(screenWidth/2), 30, exit_button.width, exit_button.height};
+
     //init bouncing ball
     Vector2 ballPosition = { screenWidth/2.0f, screenHeight/2.0f };
     Vector2 ballSpeed = { 7.0f, 7.0f };
     int ballRadius = 100;
+
     //exit.png init
     Image exit_image = LoadImage("images/exitdoor.png");
-    ImageResize(&exit_image, 100, 100);
+    ImageResize(&exit_image, 50, 50);
     Texture2D exit_texture = LoadTextureFromImage(exit_image);
+
     // trollface.png init
-    Image trollface = LoadImage("images/Trollface.png");
-    ImageDrawCircleV(&trollface, ballPosition, ballRadius, WHITE);
-    ImageResize(&trollface, ballRadius*2, ballRadius*2);
+    Image trollface = LoadImage("images/whatever.png");
+    Image circleTrans = GenImageColor(trollface.width, trollface.height, BLANK);
+    Vector2 imageCenter = { trollface.width/2.0f, trollface.height/2.0f };
+    ImageDrawCircleV(&circleTrans, imageCenter, ballRadius, WHITE);    
+    ImageAlphaMask(&trollface, circleTrans);
     Texture2D troll_texture = LoadTextureFromImage(trollface);
+
     //free exit.png memory
-    UnloadImage(exit_image);
-    
+    UnloadImage(exit_image);   
+    UnloadImage(circleTrans); 
+
     // pause menu init
     bool pause = 0;
+
     // frame counter
     int framesCounter = 0;
 
     // init paddles
     Rectangle paddleLeft = { (screenWidth-(screenWidth/16)*15), (screenHeight/2), 25, 100};
     Rectangle paddleRight = { (screenWidth-(screenWidth/16)*1), (screenHeight/2), 25, 100};
+
     // paddle bounds
     Rectangle paddleLeft_bounds = {0, 0, paddleLeft.width, paddleLeft.height};
     Rectangle paddleRight_bounds = {0, 0, paddleRight.width, paddleRight.height };
-
 
     SetTargetFPS(60); 
     
@@ -104,58 +115,60 @@ int main(void)
         if (currentScreen == StartScreen && CheckCollisionPointRec(mousePoint, startbtnbounds) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
         {
             // Transition to PlayScreen
-            currentScreen = PlayScreen;
-            
+            currentScreen = PlayScreen;            
             // Reset flags for the new screen
             startSoundPlayed = false;
             playSoundPlayed = false;
+            
         }
+        
+        
         // playscreen
         if (currentScreen == PlayScreen && CheckCollisionPointRec(mousePoint, exit_button_bounds) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
         {
             // Transition to PlayScreen
-            currentScreen = StartScreen;
-            
+            currentScreen = StartScreen;            
             // Reset flags for the new screen
             startSoundPlayed = false;
             playSoundPlayed = false;
+            
+            
+
+            
         }
 
         switch(currentScreen)
         {            
             case StartScreen:
             {
-                if (!startSoundPlayed)
-                {
-                    PlaySound(startSound);
-                    StopSound(playSound);
-                    startSoundPlayed = true;                    
-                }
-                
+                if (!IsMusicStreamPlaying(startSound))
+                    {
+                        PlayMusicStream(startSound);
+                                                       
+                    }
+
+                    UpdateMusicStream(startSound);
                 break;
             }
             case PlayScreen:
             {
-                if (!playSoundPlayed)
-                {
-                    PlaySound(playSound);
-                    StopSound(startSound);
-                    playSoundPlayed = true;                    
-                }
-
-                if(CheckCollisionPointRec(mousePoint, exit_button_bounds) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-                {
-                    currentScreen = StartScreen;
-                    playSoundPlayed = false;
-                }
+                
+                
+                
                 if (IsKeyPressed(KEY_SPACE)) pause = !pause;
 
                 if (!pause)
                 {
+                    if (!IsMusicStreamPlaying(playSound))
+                    {
+                        PlayMusicStream(playSound);
+                                                       
+                    }
+
+                    UpdateMusicStream(playSound);
                     // ball moving
                     ballPosition.x += ballSpeed.x;
-                    ballPosition.y += ballSpeed.y;                    
-
+                    ballPosition.y += ballSpeed.y;
                     // Check walls collision for bouncing
                     if ((ballPosition.x >= (GetScreenWidth() - ballRadius)) || (ballPosition.x <= ballRadius)) ballSpeed.x *= -1.0f;
                     if ((ballPosition.y >= (GetScreenHeight() - ballRadius)) || (ballPosition.y <= ballRadius)) ballSpeed.y *= -1.0f;
@@ -166,8 +179,7 @@ int main(void)
                         ballSpeed.x *= -1.0f;
                         ballSpeed.y *= 1.0f;
                         ballPosition.x = paddleLeft.x + paddleLeft.width + ballRadius;
-                        PlaySound(ballbouncefx);
-                        
+                        PlaySound(ballbouncefx);                        
                     }
                     // Right paddle
                     if (CheckCollisionCircleRec(ballPosition, ballRadius, paddleRight)) {
@@ -184,37 +196,38 @@ int main(void)
                         {
                             paddleLeft.y -= 15.0f;
                         }
-
                     }
                     // Down key function
                     if (IsKeyDown(KEY_S))
-                    {
-                        
+                    {                        
                         if (paddleLeft.y+paddleLeft.height< screenHeight)
                         {
                             paddleLeft.y += 15.0f;
                         }
-                    }
-                    
+                    }                    
                     // right paddle moving
                     if (ballPosition.y < (paddleRight.y+paddleRight.height/2))
                     {
-                        paddleRight.y -= 4.0f;
+                        paddleRight.y -= 5.5f;
                     }
                     else if (ballPosition.y > (paddleRight.y+paddleRight.height/2))
                     {
-                        paddleRight.y += 4.0f;
+                        paddleRight.y += 5.5f;
                     }
-
                     // right paddle boundary
                     if (paddleRight.y < 0) paddleRight.y=0;
                     if ((paddleRight.y+paddleRight.height) > GetScreenHeight())
                     {
-                        paddleRight.y = GetScreenHeight()-paddleRight.height;DrawTexture(troll_texture, ballPosition.x-(trollface.width/2)-ballRadius, ballPosition.y-(trollface.height/2), WHITE);
-                
+                        paddleRight.y = GetScreenHeight()-paddleRight.height;DrawTexture(troll_texture, ballPosition.x-(trollface.width/2)-ballRadius, ballPosition.y-(trollface.height/2), WHITE);                
                     }
-
                 }
+
+                if(CheckCollisionPointRec(mousePoint, exit_button_bounds) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+                {
+                    currentScreen = StartScreen;
+                    playSoundPlayed = false;
+                }
+
                 else framesCounter++;
                 break;
             }        
@@ -226,58 +239,45 @@ int main(void)
         switch(currentScreen)
         {
             case StartScreen:
-            {
-                
+            {                
                 DrawText("Welcome! \nThis is my first C program that I have made on my own. \nHave fun!", 2, 2, 50, BLACK);
-                // Used ChatGPT to help with centering button and image
-                
+                // Used ChatGPT to help with centering button and image                
                 DrawRectangle((screenWidth/2)-(startbutton.width/2), ((screenHeight/2) + 350), startbutton.width, startbutton.height, BLACK);
                 // measure text width
                 int textWidth = MeasureText("START", 100);
-                DrawText("Click To Start", (screenWidth/2)-(textWidth/2), ((screenHeight/2)+350)+(startbutton.height/2) - (100/2), 50, WHITE);
-            
+                DrawText("Click To Start", (screenWidth/2)-(textWidth/2), ((screenHeight/2)+350)+(startbutton.height/2) - (100/2), 50, WHITE);            
                 // I used ChatGPT.com to figure out how to center the image 
                 DrawTexture(programmers_texture, ((screenWidth - programmers_texture.width)/2), ((screenHeight - programmers_texture.height)/2), WHITE);
-
                 // referenced button properties from user https://www.raylib.com/examples/textures/loader.html?name=textures_sprite_button
                 break;
-            }
-            
+            }            
             case PlayScreen:
-            {
-                
+            {                
                 //Draw Exit Sign
                 DrawText("Click to Exit", (screenWidth/2), 0, 20, BLACK);
                 DrawTexture(exit_texture, (screenWidth/2), 30, WHITE);
-
                 //Draw ball
                 DrawTexture(troll_texture, ballPosition.x-(troll_texture.width/2), ballPosition.y-(troll_texture.height/2), WHITE);
-                
-
+                Texture2D troll_texture = LoadTexture("images/Trollface.png");
+                if (troll_texture.id == 0)
+                {
+                    printf("Failed to load texture.\n");
+                }
                 // Draw Paddles
                 DrawRectangle(paddleLeft.x, paddleLeft.y, paddleLeft.width, paddleLeft.height, BLACK);
                 DrawRectangle(paddleRight.x, paddleRight.y, paddleRight.width, paddleRight.height, BLACK);
-
-
-
                 break;
-
-
-
             }
         }
-
         EndDrawing();
     }
     UnloadSound(ballbouncefx);
-    UnloadSound(startSound);
-    UnloadSound(playSound);
+    UnloadMusicStream(startSound);
+    UnloadMusicStream(playSound);
     UnloadImage(trollface);
     UnloadTexture(troll_texture);
     CloseAudioDevice();
-    CloseWindow();
-    
-    
+    CloseWindow();    
     return 0;
     
 }
